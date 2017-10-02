c33ba601746e4b374506e52d9bc295915499f9f8
Harri Feng <harrifeng@gmail.com>
2017-10-02 16:59:06 +0800

    ok to try

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,14 @@
 sudo yum install -y epel-release
-sudo yum install -y python-devel python-pip gcc git tmux java-1.8.0-openjdk golang
+sudo yum install -y python-devel python-pip gcc git tmux java-1.8.0-openjdk golang nmap-ncat
 sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
+rm -rf /home/vagarnt/.ssh
+git clone https://github.com/harrifeng/ssh-in-vagrant /home/vagrant/.ssh
+
+echo "
+10.19.19.21 atom
+10.19.19.22 e6400
+10.19.19.23 e350
+" | sudo tee -a /ect/hosts
+
 echo "<----------finishing provision--------------->"
